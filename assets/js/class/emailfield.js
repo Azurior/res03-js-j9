6ae9bc0ff19695ce3985d9a19a48b3d219fd25a2
Azurior <azurior24@gmail.com>
2023-01-13 15:25:38 +0000

    update 16h30

--- assets/js/class/emailfield.js
+++ assets/js/class/emailfield.js
@@ -8,7 +8,19 @@ class EmailField extends Field {
     }
     
     validate (){
-        
+        let flag = false;
+        for(let i = 0; i < this.#field.length; i++){
+            if(this#field[i].errors.length > 0){
+                flag = true;
+            }
+        }
+        if(flag === true){
+            this.#field.classList.remove("nok");
+            this.#field.classList.add("ok");
+        }else{
+            this.#field.classList.remove("ok");
+            this.#field.classList.add("nok");
+        }
     }
 }
 
